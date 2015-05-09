76e355380d2e0f7b39cfe9f6eb50626d182dc4ce
linster <linster247@gmail.com>
2015-05-09 17:04:20 -0600

    Finally parsing string fack

--- pants/src/listener.rs
+++ pants/src/listener.rs
@@ -16,7 +16,7 @@ impl Listener {
     pub fn new (mut stream: net::TcpStream ){
         let mut tcpString: String = String::new();
         stream.read_to_string(&mut tcpString);
-        
+        Listener::ParseRequestStr(tcpString.to_string());
         println!("{}", tcpString);
 
     }
@@ -26,19 +26,20 @@ impl Listener {
     }
     
     
-    fn ParseRequestStr(self, req_str: &str) {
+    pub fn ParseRequestStr(req_str: String) {
         //Populate the fields of the listener struct
         //http://www.trevisrothwell.com/2015/01/string-tokenization-in-rust/
         //Now StrExt doesn't exist. Part of std:str 
         
-        let mut term = req_str.ToString.Split(" ");
+        //let mut terms = req_str.to_string().split(" ");
         
-        loop { //Works only if path isn't missing
-            match term.next().as_ref() {
-                "GET" => {self.reqType = "GET"},
-                path @ _ => {self.reqFile = path},
-                "HTTP /1.1" => {self.httpProto = "HTTP /1.1"}
-            }
+        for term in req_str.split(" ") { //Works only if path isn't missing
+            println!("{}",term);
+           // match *term {
+           //     String::from_str("GET") => {self.reqType = "GET".to_string()},
+           //     path @ _ => {self.reqFile = path},
+           //     String::from_str("HTTP /1.1") => {self.httpProto = "HTTP /1.1".to_string()}
+           // }
         }
     }
     
