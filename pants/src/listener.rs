d6ae8a3b1ec8ccc8ee0efffce766f1e8d6ae6bf1
linster <linster247@gmail.com>
2015-05-09 15:08:41 -0600

    Commit loop

--- pants/src/listener.rs
+++ pants/src/listener.rs
@@ -1,4 +1,5 @@
 use std::net;
+use std::str;
 
 struct Listner{
     clientAddr: net::SocketAddr,
@@ -12,5 +13,22 @@ struct Listner{
 impl Listner {
     fn listen () {
         
+    }
+    
+    
+    fn ParseRequestStr(self, req_str: str) {
+        //Populate the fields of the listener struct
+        //http://www.trevisrothwell.com/2015/01/string-tokenization-in-rust/
+        //Now StrExt doesn't exist. Part of std:str 
+        
+        let mut term = str::Split(req_str, " ");
+        
+        loop {
+            match term.next() {
+                "GET" => {self.reqType = "GET"},
+                path @ _ => {self.reqFile = path},
+                "HTTP /1.1" => {self.httpProto = "HTTP /1.1"}
+            }
+        }
     }
 }
