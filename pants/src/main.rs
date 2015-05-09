09f96039361ae60a3df6cd85ca565f49173a1640
Grant Hunter <gr@nt-hunter.ca>
2015-05-09 14:04:28 -0600

    added threadpool dependency

--- pants/src/main.rs
+++ pants/src/main.rs
@@ -1,3 +1,25 @@
+extern crate threadpool;
+
+use threadpool::ThreadPool;
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    const MAX_THREADS: usize = 10;
+    
+    //create input pool
+    let pool = ThreadPool::new(MAX_THREADS);
+    
+    for i in 0..5 {
+        println!("{}",i);
+        pool.execute(move || print(i));
+    }
+    
+    //create output threads
+
+}
+
+fn print(i:u32){
+    println!("hello world! {}", i);
 }
