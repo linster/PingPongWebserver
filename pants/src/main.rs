97c9fe13d5b0f80312cffb436f44b054e25aa047
Grant Hunter <gr@nt-hunter.ca>
2015-05-16 14:35:03 -0600

    created a shared buffer and started making the response threads

--- pants/src/main.rs
+++ pants/src/main.rs
@@ -1,34 +1,69 @@
-use std::net::TcpListener;
+use std::net::{TcpListener,TcpStream};
 use std::thread;
+use std::sync::{Arc, Mutex};
 use request::Request;
+use std::io::Write;
 
 mod request;
 
 
 fn main() {
     
-     const TCP_ADDR: &'static str = "127.0.0.1:8080";
-    //create input pool
+    const TCP_ADDR: &'static str = "127.0.0.1:8080"; 
+    
+    let request_vec: Vec<Request> = Vec::new();
+    let mutex = Mutex::new(request_vec);
+    let atomic = Arc::new(mutex);
+     
+    //copy pointers to move into threads later
+    let req = atomic.clone();
+    let res = atomic.clone();
     
     
-    let listener = TcpListener::bind(TCP_ADDR).unwrap();
-    
-    for stream in listener.incoming(){
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move|| {
-                     let req = Request::new(stream); 
-                     println!("{}", req.client_addr)
-                });
-            }
-            Err(e) => {
-                println!("thread {}", e);
+    //Request threads
+     thread::spawn(move|| {
+        //create input pool
+        let listener = TcpListener::bind(TCP_ADDR).unwrap();
+
+        for stream in listener.incoming(){
+            match stream {
+                Ok(stream) => {
+                    println!("incomming request");
+                    let request_vec = req.clone();
+                    
+                    thread::spawn(move|| {
+                        println!("in request thread");
+                         let req = Request::new(stream); 
+                         request_vec.lock().unwrap().push(req);
+                    });
+                }
+                Err(e) => {
+                    println!("thread {}", e);
+                }
             }
         }
-    }
+     });
+    
+    //Response threads
+    thread::spawn(move|| {
+        let request_vec = res.clone();
+        
+        loop {
+            if request_vec.lock().unwrap().is_empty() {
+                thread::yield_now();
+            } else {
+                println!("in request thread");
+                let request = request_vec.lock().unwrap().pop().unwrap();
+                thread::spawn(move|| {
+                     let mut stream = TcpStream::connect(request.client_addr).unwrap();
+                     let _ = stream.write("hello world!".to_string().as_bytes());
+                     println!("lol")
+                });
+            }
+        }
+    });
     
     
-    //create output threads
-
+    loop {}
     
 }
